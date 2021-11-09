35675d0eed3179bb6eaf0e2beb31ef3e6f8f213d
take-tech1001 <tk.tech1001@gmail.com>
2021-11-09 23:54:08 +0900

    🔧 不要な記述削除

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  rewrites: async () => {
-    return [{ source: "/", destination: "/root" }];
-  },
-  i18n: { locales: ["ja"], defaultLocale: "ja" },
   future: { strictPostcssConfiguration: true },
   pageExtensions: ["page.tsx", "page.ts"],
   reactStrictMode: true,
