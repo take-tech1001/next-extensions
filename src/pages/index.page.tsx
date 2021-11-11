b9b2a31a967c186caa34e46d49fb315f0a8212a8
take-tech1001 <tk.tech1001@gmail.com>
2021-11-11 23:59:46 +0900

    ✨

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
+import React, { useRef, useState } from 'react'
 
-const Home: NextPage = () => {
+const Root: NextPage = () => {
   return (
-    <>
-      <p className="text-red-900 font-bold">Home</p>
-    </>
+    <div className="relative w-[600px] h-[550px]">
+      <div className="absolute top-0 left-0">
+        <iframe
+          src="https://nextjs.org/docs/getting-started"
+          width="600"
+          height="550"
+        ></iframe>
+      </div>
+    </div>
   )
 }
 
-export default Home
+export default Root
