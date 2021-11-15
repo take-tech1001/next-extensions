d1638d7c748169b70731436c89338b44c0312e4a
take-tech1001 <tk.tech1001@gmail.com>
2021-11-15 22:59:55 +0900

    ✨ コンテキストメニューに追加

--- extensions/background.js
+++ extensions/background.js
@@ -1,3 +1,20 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('onInstalled')
+  chrome.contextMenus.create({
+    type: 'normal',
+    id: 'next-extension',
+    title: 'Next.js Extension',
+    contexts: ['all'],
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  console.log(info)
+  if (info.menuItemId === 'next-extension') {
+    chrome.windows.create({
+      url: 'https://nextjs.org/docs/getting-started',
+      type: 'popup',
+      width: 700,
+      height: 700,
+    })
+  }
 })
